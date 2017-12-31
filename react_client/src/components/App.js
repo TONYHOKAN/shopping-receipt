1fa57d548afa1372bcc8c604fab84acafa1e5bc0
tony ng <tony.hokan@gmail.com>
2017-12-31 20:44:21 +0800

    Main layout for interactive UI. We will have left side control panel and right side data display panel.

--- react_client/src/components/App.js
+++ react_client/src/components/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
+import { Card, CardHeader } from 'reactstrap'
 import './App.css'
+
+const style = {
+  leftPanel: {
+    flex: 1,
+    backgroundColor: '#dee2e6'
+  },
+  rightPanel: {
+    flex: 3
+  },
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  productList: {
+    flex: 1
+  },
+  shoppingCart: {
+    flex: 1
+  },
+  receipt: {
+    flex: 1
+  }
+}
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ display: 'flex', height: '100%' }}>
+        <div style={style.leftPanel}>
+
+        </div>
+        <div style={style.rightPanel}>
+          <div style={style.container}>
+            <Card style={style.productList}>
+              <CardHeader>Product List</CardHeader>
+            </Card>
+            <Card style={style.shoppingCart}>
+              <CardHeader>Shopping Cart</CardHeader>
+            </Card>
+            <Card style={style.receipt}>
+              <CardHeader>Shopping Receipt</CardHeader>
+            </Card>
+          </div>
+        </div>
       </div>
     )
   }

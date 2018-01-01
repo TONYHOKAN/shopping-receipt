7a19bea6f28d2f034e9e3f3254afdf4016444caf
tony ng <tony.hokan@gmail.com>
2018-01-01 12:29:02 +0800

    Update layout of Receipt.js.

--- react_client/src/components/Receipt.js
+++ react_client/src/components/Receipt.js
@@ -4,6 +4,21 @@ import PropTypes from 'prop-types'
 
 import { calculateCartSubTotalTaxTotal } from '../services/receipt'
 
+const style = {
+  itemColumn: {
+    border: 'none',
+    textAlign: 'left'
+  },
+  priceColumn: {
+    border: 'none',
+    textAlign: 'right'
+  },
+  qtyColumn: {
+    border: 'none',
+    textAlign: 'right'
+  }
+}
+
 const Receipt = (props) => {
   const { location, shoppingCart } = props
   if (Object.keys(shoppingCart).length === 0) {
@@ -14,35 +29,35 @@ const Receipt = (props) => {
     <Table size="sm" className="borderless">
       <thead style={{ border: 'none' }}>
         <tr>
-          <th style={{ border: 'none' }}>item</th>
-          <th style={{ border: 'none' }}>price</th>
-          <th style={{ border: 'none' }}>qty</th>
+          <th style={style.itemColumn}>item</th>
+          <th style={style.priceColumn}>price</th>
+          <th style={style.qtyColumn}>qty</th>
         </tr>
       </thead>
       <tbody>
         {Object.keys(shoppingCart).map(key => {
           return (
             <tr key={`cart-item_${key}`}>
-              <td style={{ border: 'none' }}>{shoppingCart[key].name}</td>
-              <td style={{ border: 'none' }}>{shoppingCart[key].price.toFixed(2)}</td>
-              <td style={{ border: 'none' }}>{shoppingCart[key].qty}</td>
+              <td style={style.itemColumn}>{shoppingCart[key].name}</td>
+              <td style={style.priceColumn}>{shoppingCart[key].price.toFixed(2)}</td>
+              <td style={style.qtyColumn}>{shoppingCart[key].qty}</td>
             </tr>
           )
         })}
         <tr>
-          <td style={{ border: 'none' }}>subtotal:</td>
-          <td style={{ border: 'none' }}></td>
-          <td style={{ border: 'none' }}>{`$${subtotal.toFixed(2)}`}</td>
+          <td style={style.itemColumn}>subtotal:</td>
+          <td style={style.priceColumn}></td>
+          <td style={style.qtyColumn}>{`$${subtotal.toFixed(2)}`}</td>
         </tr>
         <tr>
-          <td style={{ border: 'none' }}>tax:</td>
-          <td style={{ border: 'none' }}></td>
-          <td style={{ border: 'none' }}>{`$${tax.toFixed(2)}`}</td>
+          <td style={style.itemColumn}>tax:</td>
+          <td style={style.priceColumn}></td>
+          <td style={style.qtyColumn}>{`$${tax.toFixed(2)}`}</td>
         </tr>
         <tr>
-          <td style={{ border: 'none' }}>total:</td>
-          <td style={{ border: 'none' }}></td>
-          <td style={{ border: 'none' }}>{`$${total.toFixed(2)}`}</td>
+          <td style={style.itemColumn}>total:</td>
+          <td style={style.priceColumn}></td>
+          <td style={style.qtyColumn}>{`$${total.toFixed(2)}`}</td>
         </tr>
       </tbody>
     </Table>
